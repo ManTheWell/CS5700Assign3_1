73e8f85c90d7b5299f0a96f804e581de01c585e2
smengst <a02304220@usu.edu>
2025-08-08 00:28:43 -0600

    Done

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.9.0"
+    id("org.jetbrains.compose") version "1.5.11"
     kotlin("plugin.serialization") version "1.9.0"
 }
 
@@ -18,30 +19,34 @@ repositories {
 dependencies {
     implementation(compose.desktop.currentOs)
 
+    // Ktor server
     implementation("io.ktor:ktor-server-core:2.3.7")
     implementation("io.ktor:ktor-server-netty:2.3.7")
     implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
-
-    implementation("io.ktor:ktor-server-core:2.3.7")
-    implementation("io.ktor:ktor-server-netty:2.3.7")
-    implementation("io.ktor:ktor-server-content-negotiation:2.3.7")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("ch.qos.logback:logback-classic:1.2.11")
-    implementation("org.junit.jupiter:junit-jupiter:5.8.1")
-
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.7")
-    testImplementation(kotlin("test"))
-
     implementation("io.ktor:ktor-server-cors:2.3.9")
+    implementation("io.ktor:ktor-server-websockets:2.3.7")
+
+    // Ktor client
     implementation("io.ktor:ktor-client-core:2.3.7")
-    implementation("io.ktor:ktor-client-cio:2.3.7") // or OkHttp
+    implementation("io.ktor:ktor-client-cio:2.3.7")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.7")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.7")
 
-    implementation("io.ktor:ktor-server-websockets:2.3.7") // WebSocket support
+    // JSON serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+
+    // Logging
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+
+    // Testing
+    testImplementation(kotlin("test"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.7")
+}
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.jvmTarget = "17" // or "1.8" if you're using Java 8
 }
 
 tasks.test {
